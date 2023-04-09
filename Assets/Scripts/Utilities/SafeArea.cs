45e6416bf2a6c64f00179ef598abea0da4331515
Shahari93 <shaharilani93@gmail.com>
2023-04-09 11:09:13 +0300

    Initial commit Safe area for notch devices

---
+++ Assets/Scripts/Utilities/SafeArea.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Dots.Utils.SafeArea
+{
+	public class SafeArea : MonoBehaviour
+	{
+		RectTransform rectTransform;
+		Rect safeArea;
+
+		Vector2 minAnchor;
+		Vector2 maxAnchor;
+
+        private void Awake()
+        {
+            
+        }
+    } 
+}
