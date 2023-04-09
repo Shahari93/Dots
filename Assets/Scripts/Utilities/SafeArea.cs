56768a0b175f69beda8dc2112c7a3c0691ce7e83
Shahari93 <shaharilani93@gmail.com>
2023-04-09 11:16:32 +0300

    Working safe area script

--- Assets/Scripts/Utilities/SafeArea.cs
+++ Assets/Scripts/Utilities/SafeArea.cs
@@ -12,7 +12,26 @@ namespace Dots.Utils.SafeArea
 
         private void Awake()
         {
-            
+            SetSafeArea();
+        }
+
+        private void SetSafeArea()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            safeArea = Screen.safeArea;
+
+            minAnchor = safeArea.position;
+            maxAnchor = minAnchor + safeArea.size;
+
+            minAnchor.x /= Screen.width;
+            minAnchor.y /= Screen.height;
+
+            maxAnchor.x /= Screen.width;
+            maxAnchor.y /= Screen.height;
+
+
+            rectTransform.anchorMin = minAnchor;
+            rectTransform.anchorMax = maxAnchor;
         }
     } 
 }
