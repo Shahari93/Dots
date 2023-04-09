7ccb6861076ab6cfa40ab2c865c8480b9650aa83
Shahari93 <shaharilani93@gmail.com>
2023-04-09 18:54:03 +0300

    Player dots rotate around a center point Each tap on the screen change the rotation angle by multiplying with -1

---
+++ Assets/Scripts/Player/DotsRotation.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Dots.GamePlay.Player
+{
+    public class DotsRotation : MonoBehaviour
+    {
+        [SerializeField] float rotationDegree;
+        [SerializeField] float rotationSpeed;
+
+        void Update()
+        {
+            RotateDotsAround(rotationDegree);
+            if (Input.touchCount == 1)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    rotationDegree *= -1f;
+                }
+            }
+        }
+
+        private void RotateDotsAround(float degree)
+        {
+            transform.RotateAround(transform.position, transform.forward, degree * Time.deltaTime * rotationSpeed);
+        }
+    }
+}
