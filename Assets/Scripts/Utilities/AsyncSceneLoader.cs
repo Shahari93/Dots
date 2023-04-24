cba97c6aeb19d5e41bf05f510b26e4cb33b144e1
Shahari93 <shaharilani93@gmail.com>
2023-04-24 11:36:56 +0300

    Added a starting scene with play button

---
+++ Assets/Scripts/Utilities/AsyncSceneLoader.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+namespace Dots.Utils.SceneLoader
+{
+	public class AsyncSceneLoader : MonoBehaviour
+	{
+		[SerializeField] Button loadSceneButton;
+
+        void Start()
+        {
+			loadSceneButton.onClick.AddListener(OnPlayButtonPressed);
+        }
+
+		private void OnPlayButtonPressed()
+		{
+			StartCoroutine(LoadAsyncScene());
+		}
+
+        IEnumerator LoadAsyncScene()
+		{
+			AsyncOperation asyncSceneLoad = SceneManager.LoadSceneAsync("Dots_Gameplay", LoadSceneMode.Single);
+			while (!asyncSceneLoad.isDone)
+			{
+				yield return null;
+			}
+		}
+	} 
+}
