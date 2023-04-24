e36146e1f8eddb01aa56986f090548e814ddc4b8
Shahari93 <shaharilani93@gmail.com>
2023-04-24 12:07:01 +0300

    Created delete playerprefs editor utility

---
+++ Assets/Scripts/Utilities/DeletePlayerPrefsEditor.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Dots.Utils.DeletePlayerPrefs
+{
+    public class DeletePlayerPrefsScript : EditorWindow
+    {
+        [MenuItem("Window/Delete PlayerPrefs (All)")]
+        static void DeleteAllPlayerPrefs()
+        {
+            if(EditorUtility.DisplayDialog("Clear PlayerPrefs?", "Are you sure you want to clear PlayerPrefs? This can't be undone ", "Yes", "No"))
+            {
+                PlayerPrefs.DeleteAll();
+                Debug.Log("Player prefs deleted");
+            }
+
+        }
+    } 
+}
