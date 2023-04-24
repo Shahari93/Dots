e23fa3993ce76910750f45566bfec5f6c1f55e01
Shahari93 <shaharilani93@gmail.com>
2023-04-24 13:56:25 +0300

    preprocessor directives for delete playerprefs

--- Assets/Scripts/Utilities/DeletePlayerPrefsEditor.cs
+++ Assets/Scripts/Utilities/DeletePlayerPrefsEditor.cs
@@ -3,17 +3,19 @@ using UnityEditor;
 
 namespace Dots.Utils.DeletePlayerPrefs
 {
+#if UNITY_EDITOR
     public class DeletePlayerPrefsScript : EditorWindow
     {
         [MenuItem("Window/Delete PlayerPrefs (All)")]
         static void DeleteAllPlayerPrefs()
         {
-            if(EditorUtility.DisplayDialog("Clear PlayerPrefs?", "Are you sure you want to clear PlayerPrefs? This can't be undone ", "Yes", "No"))
+            if (EditorUtility.DisplayDialog("Clear PlayerPrefs?", "Are you sure you want to clear PlayerPrefs? This can't be undone ", "Yes", "No"))
             {
                 PlayerPrefs.DeleteAll();
                 Debug.Log("Player prefs deleted");
             }
 
         }
-    } 
+    }  
+#endif
 }
