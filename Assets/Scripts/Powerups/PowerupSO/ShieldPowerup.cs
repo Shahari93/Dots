ee9f84477c6afea9582026cc7599f0b096373935
Shahari93 <shaharilani93@gmail.com>
2023-05-09 10:01:13 +0300

    Not throwing an error for the unimplemented powerups

--- Assets/Scripts/Powerups/PowerupSO/ShieldPowerup.cs
+++ Assets/Scripts/Powerups/PowerupSO/ShieldPowerup.cs
@@ -6,6 +6,6 @@ public class ShieldPowerup : PowerupEffectSO
 {
     public override void Apply(GameObject target)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Hit");
     }
 }
--- Assets/Scripts/Powerups/PowerupSO/SlowSpeedPowerup.cs
+++ Assets/Scripts/Powerups/PowerupSO/SlowSpeedPowerup.cs
@@ -5,6 +5,6 @@ public class SlowSpeedPowerup : PowerupEffectSO
 {
     public override void Apply(GameObject target)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Hit");
     }
 }
