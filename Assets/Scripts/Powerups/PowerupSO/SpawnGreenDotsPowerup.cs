59f9f121ea8ffd651bf1ebe37b1859f0ffb43393
Shahari93 <shaharilani93@gmail.com>
2023-05-09 11:42:48 +0300

    Working on implementing each powerup ability

--- Assets/Scripts/Powerups/PowerupSO/SpawnGreenDotsPowerup.cs
+++ Assets/Scripts/Powerups/PowerupSO/SpawnGreenDotsPowerup.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
-using Dots.GamePlay.Dot.Good;
 using Dots.Utils.Destroy;
+using Dots.GamePlay.Dot.Good;
 
 namespace Dots.GamePlay.Powerups.SpawnGreens
 {
@@ -15,6 +15,7 @@ namespace Dots.GamePlay.Powerups.SpawnGreens
             Debug.Log("Hit");
             GoodDot.spawnChance = 1f;
             DestroingPowerup.OnCollectedPower?.Invoke(powerupDuration);
+            InvokePowerupUI?.Invoke(powerupDuration);
         }
-    } 
+    }
 }
