d8f6b547bf38a5430cca093651d19e9493f1bc69
Shahari93 <shaharilani93@gmail.com>
2023-05-10 17:21:57 +0300

    Possible fix for Error when hitting a powerup after restart level

--- Assets/Scripts/UI/PowerupDisplay.cs
+++ Assets/Scripts/UI/PowerupDisplay.cs
@@ -1,51 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
 using Dots.GamePlay.Powerups;
 using System;
 using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
 
 public class PowerupDisplay : MonoBehaviour
 {
+    private bool shouldEnableImage;
+    private float powerupDuration = 5;
     [SerializeField] Image powerupDurationImage;
 
     private void OnEnable()
     {
+        powerupDurationImage.fillAmount = 1;
         PowerupEffectSO.InvokePowerupUI += EnablePowerupDurationDisplay;
     }
 
-    private void Awake()
+    private void Start()
     {
-        powerupDurationImage.gameObject.SetActive(false);
-        powerupDurationImage.fillAmount = 1;
+        StartCoroutine(EnablePowerupDurationCoroutine(powerupDuration));
+    }
+
+    private IEnumerator EnablePowerupDurationCoroutine(float duration)
+    {
+        if (shouldEnableImage)
+        {
+            powerupDurationImage.enabled = true;
+            while (powerupDuration > 0)
+            {
+                powerupDuration -= Time.deltaTime;
+                powerupDurationImage.fillAmount -= 1 / powerupDuration * Time.deltaTime;
+            }
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     private void EnablePowerupDurationDisplay(float duration)
     {
-        if (duration > 0)
-        {
-            powerupDurationImage.gameObject.SetActive(true);
-            StartCoroutine(StartPowerupDurationCoroutine(duration));
-        }
-    }
-
-    private IEnumerator StartPowerupDurationCoroutine(float duration)
-    {
-        duration -= Time.deltaTime;
-        while (duration > 0)
-        {
-            powerupDurationImage.fillAmount = 1 / duration * Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-            if (duration <= 0)
-            {
-                powerupDurationImage.fillAmount = 1;
-                powerupDurationImage.gameObject.SetActive(false);
-                break;
-            }
-        }
+        shouldEnableImage = true;
     }
 
     private void OnDisable()
     {
-        PowerupEffectSO.InvokePowerupUI += EnablePowerupDurationDisplay;
+        PowerupEffectSO.InvokePowerupUI -= EnablePowerupDurationDisplay;
     }
 }
