46ee1272b0e07511565f2938f5fafffa24ed46a9
Shahari93 <shaharilani93@gmail.com>
2023-05-15 13:58:53 +0300

    Interstitial works after lose game Bug: Interstitial works only one time

--- Assets/Scripts/Ads/IronSourceInit.cs
+++ Assets/Scripts/Ads/IronSourceInit.cs
@@ -1,5 +1,5 @@
-using System;
 using UnityEngine;
+using Dots.GamePlay.Dot.Bad;
 
 namespace Dots.Ads.Init
 {
@@ -11,14 +11,52 @@ namespace Dots.Ads.Init
 string appKey = "19f99b595";
 #endif
 
+        public static IronSourceInit Instance;
+
         void OnEnable()
         {
             IronSourceEvents.onSdkInitializationCompletedEvent += SdkInitializationCompletedEvent;
+
+            //Interstitial 
+            IronSourceInterstitialEvents.onAdReadyEvent += InterstitialOnAdReadyEvent;
+            IronSourceInterstitialEvents.onAdLoadFailedEvent += InterstitialOnAdLoadFailed;
+            IronSourceInterstitialEvents.onAdOpenedEvent += InterstitialOnAdOpenedEvent;
+            IronSourceInterstitialEvents.onAdClickedEvent += InterstitialOnAdClickedEvent;
+            IronSourceInterstitialEvents.onAdShowSucceededEvent += InterstitialOnAdShowSucceededEvent;
+            IronSourceInterstitialEvents.onAdShowFailedEvent += InterstitialOnAdShowFailedEvent;
+            IronSourceInterstitialEvents.onAdClosedEvent += InterstitialOnAdClosedEvent;
+
+            BadDot.OnLoseGame += ShowInterstitialAd;
+        }
+
+        private void ShowInterstitialAd()
+        {
+            if (IronSource.Agent.isInterstitialReady())
+            {
+                IronSource.Agent.showInterstitial();
+            }
+            else
+            {
+                Debug.Log("Ad is not ready");
+                return;
+            }
+        }
+
+        void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
 
         private void SdkInitializationCompletedEvent()
         {
             IronSource.Agent.validateIntegration();
+            IronSource.Agent.loadInterstitial();
         }
 
         void Start()
@@ -33,6 +71,41 @@ string appKey = "19f99b595";
         {
             IronSource.Agent.onApplicationPause(isPaused);
         }
+
+        // Interstitial Callbacks
+        // Invoked when the interstitial ad was loaded succesfully.
+        void InterstitialOnAdReadyEvent(IronSourceAdInfo adInfo)
+        {
+            Debug.Log("Ad is ready");
+        }
+        // Invoked when the initialization process has failed.
+        void InterstitialOnAdLoadFailed(IronSourceError ironSourceError)
+        {
+        }
+        // Invoked when the Interstitial Ad Unit has opened. This is the impression indication. 
+        void InterstitialOnAdOpenedEvent(IronSourceAdInfo adInfo)
+        {
+        }
+        // Invoked when end user clicked on the interstitial ad
+        void InterstitialOnAdClickedEvent(IronSourceAdInfo adInfo)
+        {
+        }
+        // Invoked when the ad failed to show.
+        void InterstitialOnAdShowFailedEvent(IronSourceError ironSourceError, IronSourceAdInfo adInfo)
+        {
+        }
+        // Invoked when the interstitial ad closed and the user went back to the application screen.
+        void InterstitialOnAdClosedEvent(IronSourceAdInfo adInfo)
+        {
+        }
+        // Invoked before the interstitial ad was opened, and before the InterstitialOnAdOpenedEvent is reported.
+        // This callback is not supported by all networks, and we recommend using it only if  
+        // it's supported by all networks you included in your build. 
+        void InterstitialOnAdShowSucceededEvent(IronSourceAdInfo adInfo)
+        {
+        }
+
+        // Rewarded Ads Callbacks
 
     }
 }
