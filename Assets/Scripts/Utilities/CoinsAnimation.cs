810384821179eb1cfd570a06dd23906f651482f2
Shahari93 <shaharilani93@gmail.com>
2023-05-17 19:47:23 +0300

    Coins text panel on end games shows the correct coins value Flying coins animation works Spawn up to 10 coins (based on how much green dots the player collected - 2 dots = 2 coins animation, 10 or above = 10 coins

--- Assets/Scripts/Utilities/CoinsAnimation.cs
+++ Assets/Scripts/Utilities/CoinsAnimation.cs
@@ -1,85 +1,98 @@
+using TMPro;
+using UnityEngine;
 using DG.Tweening;
 using Dots.Coins.Model;
 using System.Collections;
-using TMPro;
-using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
 
-public class CoinsAnimation : MonoBehaviour
+namespace Dots.Utils.CoinsAnimation
 {
-    private int coinsAmount;
-
-    [SerializeField] private GameObject pileOfCoins;
-    [SerializeField] private Transform target;
-    [SerializeField] private TMP_Text coinsAmountText;
-    [SerializeField] private Vector2[] initialPos;
-    [SerializeField] private Quaternion[] initialRotation;
-
-    // TODO: Make pileOfCoins have number of children like the CoinsToAdd from the model (No more then 10)
-    void Start()
+    public class CoinsAnimation : MonoBehaviour
     {
-        if (coinsAmount == 0)
-            coinsAmount = 10;
+        private int coinsAmount;
 
-        initialPos = new Vector2[coinsAmount];
-        initialRotation = new Quaternion[coinsAmount];
+        [SerializeField] private GameObject coin;
+        [SerializeField] private GameObject pileOfCoins;
+        [SerializeField] private Transform target;
+        [SerializeField] private TMP_Text coinsAmountText;
+        [SerializeField] private Button restartGameButton;
 
-        for (int i = 0; i < pileOfCoins.transform.childCount; i++)
+        [SerializeField] private Vector2[] initialPos;
+        [SerializeField] private Quaternion[] initialRotation;
+
+        void Start()
         {
-            initialPos[i] = pileOfCoins.transform.GetChild(i).position;
-            initialRotation[i] = pileOfCoins.transform.GetChild(i).GetComponent<RectTransform>().rotation;
-        }
+            restartGameButton.interactable = false;
+            coinsAmount = CoinsModel.CoinsToAdd;
 
-        CountCoins();
-    }
+            if(coinsAmount >= 10)
+            {
+                coinsAmount = 10;
+            }
 
-    private void ResetInitValues()
-    {
-        for (int i = 0; i < pileOfCoins.transform.childCount; i++)
-        {
-            pileOfCoins.transform.GetChild(i).position = initialPos[i];
-            pileOfCoins.transform.GetChild(i).GetComponent<RectTransform>().rotation = initialRotation[i];
-        }
-    }
+            for (int i = 0; i < coinsAmount; i++)
+            {
+                GameObject coinsChildren = Instantiate(coin, pileOfCoins.transform);
 
-    private void CountCoins()
-    {
-        if (CoinsModel.CoinsToAdd <= 0)
-            return;
+            }
 
-        else
-        {
-            ResetInitValues();
-
-            pileOfCoins.SetActive(true);
-
-            var delay = 0f;
+            initialPos = new Vector2[coinsAmount];
+            initialRotation = new Quaternion[coinsAmount];
 
             for (int i = 0; i < pileOfCoins.transform.childCount; i++)
             {
-                pileOfCoins.transform.GetChild(i).DOScale(1f, 0.3f).SetDelay(delay + 0.1f).SetEase(Ease.OutBack);
-
-                pileOfCoins.transform.GetChild(i).DOMove(target.position, 0.8f)
-                    .SetDelay(delay + 0.5f).SetEase(Ease.InBack);
-
-
-                pileOfCoins.transform.GetChild(i).DORotate(Vector3.zero, 0.5f).SetDelay(delay + 0.5f)
-                    .SetEase(Ease.Flash);
-
-
-                pileOfCoins.transform.GetChild(i).DOScale(0f, 0.3f).SetDelay(delay + 1.5f).SetEase(Ease.OutBack);
-
-                delay += 0.1f;
-
-                coinsAmountText.transform.parent.GetChild(0).transform.DOScale(1.1f, 0.1f).SetLoops(10, LoopType.Yoyo).SetEase(Ease.InOutSine).SetDelay(1.2f);
+                initialPos[i] = pileOfCoins.transform.GetChild(i).position;
+                initialRotation[i] = pileOfCoins.transform.GetChild(i).GetComponent<RectTransform>().rotation;
             }
 
-            StartCoroutine(CountDollars());
+            CountCoins();
         }
-    }
 
-    IEnumerator CountDollars()
-    {
-        yield return new WaitForSecondsRealtime(0.5f);
-        coinsAmountText.text = CoinsModel.CurrentCoinsAmount.ToString();
+        private void ResetInitValues()
+        {
+            for (int i = 0; i < pileOfCoins.transform.childCount; i++)
+            {
+                pileOfCoins.transform.GetChild(i).position = initialPos[i];
+                pileOfCoins.transform.GetChild(i).GetComponent<RectTransform>().rotation = initialRotation[i];
+            }
+        }
+
+        private async void CountCoins()
+        {
+            await Task.Delay(1000);
+            restartGameButton.interactable = true;
+
+            if (CoinsModel.CoinsToAdd <= 0)
+                return;
+
+            else
+            {
+                ResetInitValues();
+
+                pileOfCoins.SetActive(true);
+
+                var delay = 0f;
+
+                for (int i = 0; i < pileOfCoins.transform.childCount; i++)
+                {
+                    pileOfCoins.transform.GetChild(i).DOScale(1f, 0.3f).SetDelay(delay + 0.1f).SetEase(Ease.OutBack);
+
+                    pileOfCoins.transform.GetChild(i).DOMove(target.position, 0.8f)
+                        .SetDelay(delay + 0.5f).SetEase(Ease.InBack);
+
+
+                    pileOfCoins.transform.GetChild(i).DORotate(Vector3.zero, 0.5f).SetDelay(delay + 0.5f)
+                        .SetEase(Ease.Flash);
+
+
+                    pileOfCoins.transform.GetChild(i).DOScale(0f, 0.3f).SetDelay(delay + 1.5f).SetEase(Ease.OutBack);
+
+                    delay += 0.1f;
+
+                    coinsAmountText.transform.parent.GetChild(0).transform.DOScale(1.1f, 0.1f).SetLoops(10, LoopType.Yoyo).SetEase(Ease.InOutSine).SetDelay(1.2f);
+                }
+            }
+        }
     }
 }
