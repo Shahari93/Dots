7401ddab89853254a2de54c7cbde062b3243877f
Shahari93 <shaharilani93@gmail.com>
2023-05-18 19:33:45 +0300

    Disabled ads

--- Assets/Scripts/Ads/IronSourceInit.cs
+++ Assets/Scripts/Ads/IronSourceInit.cs
@@ -1,226 +1,224 @@
-using UnityEngine;
-using UnityEngine.UI;
-using Dots.GamePlay.Dot.Bad;
-using UnityEngine.SceneManagement;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-namespace Dots.Ads.Init
-{
-    public class IronSourceInit : MonoBehaviour
-    {
-#if UNITY_ANDROID
-        string appKey = "19f28384d";
-#elif Unity_iOS
-string appKey = "19f99b595";
-#endif
+//namespace Dots.Ads.Init
+//{
+//    public class IronSourceInit : MonoBehaviour
+//    {
+//#if UNITY_ANDROID
+//        string appKey = "19f28384d";
+//#elif Unity_iOS
+//string appKey = "19f99b595";
+//#endif
 
-        public static IronSourceInit Instance;
-        public const string COINS_PLACEMENT = "coins";
-        public const string SHIELD_PLACEMENT = "shield";
-        [SerializeField] Button coinsRVButton, shieldRVButton;
+//        public static IronSourceInit Instance;
+//        public const string COINS_PLACEMENT = "coins";
+//        public const string SHIELD_PLACEMENT = "shield";
+//        [SerializeField] Button coinsRVButton, shieldRVButton;
 
-        void OnEnable()
-        {
-            IronSourceEvents.onSdkInitializationCompletedEvent += SdkInitializationCompletedEvent;
+//        void OnEnable()
+//        {
+//            IronSourceEvents.onSdkInitializationCompletedEvent += SdkInitializationCompletedEvent;
 
-            //Interstitial 
-            IronSourceInterstitialEvents.onAdReadyEvent += InterstitialOnAdReadyEvent;
-            IronSourceInterstitialEvents.onAdLoadFailedEvent += InterstitialOnAdLoadFailed;
-            IronSourceInterstitialEvents.onAdOpenedEvent += InterstitialOnAdOpenedEvent;
-            IronSourceInterstitialEvents.onAdClickedEvent += InterstitialOnAdClickedEvent;
-            IronSourceInterstitialEvents.onAdShowSucceededEvent += InterstitialOnAdShowSucceededEvent;
-            IronSourceInterstitialEvents.onAdShowFailedEvent += InterstitialOnAdShowFailedEvent;
-            IronSourceInterstitialEvents.onAdClosedEvent += InterstitialOnAdClosedEvent;
+//            //Interstitial 
+//            IronSourceInterstitialEvents.onAdReadyEvent += InterstitialOnAdReadyEvent;
+//            IronSourceInterstitialEvents.onAdLoadFailedEvent += InterstitialOnAdLoadFailed;
+//            IronSourceInterstitialEvents.onAdOpenedEvent += InterstitialOnAdOpenedEvent;
+//            IronSourceInterstitialEvents.onAdClickedEvent += InterstitialOnAdClickedEvent;
+//            IronSourceInterstitialEvents.onAdShowSucceededEvent += InterstitialOnAdShowSucceededEvent;
+//            IronSourceInterstitialEvents.onAdShowFailedEvent += InterstitialOnAdShowFailedEvent;
+//            IronSourceInterstitialEvents.onAdClosedEvent += InterstitialOnAdClosedEvent;
 
-            //Add AdInfo Rewarded Video Events
-            IronSourceRewardedVideoEvents.onAdOpenedEvent += RewardedVideoOnAdOpenedEvent;
-            IronSourceRewardedVideoEvents.onAdClosedEvent += RewardedVideoOnAdClosedEvent;
-            IronSourceRewardedVideoEvents.onAdAvailableEvent += RewardedVideoOnAdAvailable;
-            IronSourceRewardedVideoEvents.onAdUnavailableEvent += RewardedVideoOnAdUnavailable;
-            IronSourceRewardedVideoEvents.onAdShowFailedEvent += RewardedVideoOnAdShowFailedEvent;
-            IronSourceRewardedVideoEvents.onAdRewardedEvent += RewardedVideoOnAdRewardedEvent;
-            IronSourceRewardedVideoEvents.onAdClickedEvent += RewardedVideoOnAdClickedEvent;
+//            //Add AdInfo Rewarded Video Events
+//            IronSourceRewardedVideoEvents.onAdOpenedEvent += RewardedVideoOnAdOpenedEvent;
+//            IronSourceRewardedVideoEvents.onAdClosedEvent += RewardedVideoOnAdClosedEvent;
+//            IronSourceRewardedVideoEvents.onAdAvailableEvent += RewardedVideoOnAdAvailable;
+//            IronSourceRewardedVideoEvents.onAdUnavailableEvent += RewardedVideoOnAdUnavailable;
+//            IronSourceRewardedVideoEvents.onAdShowFailedEvent += RewardedVideoOnAdShowFailedEvent;
+//            IronSourceRewardedVideoEvents.onAdRewardedEvent += RewardedVideoOnAdRewardedEvent;
+//            IronSourceRewardedVideoEvents.onAdClickedEvent += RewardedVideoOnAdClickedEvent;
 
 
-            //BadDot.OnLoseGame += ShowInterstitialAd;
-        }
+//            //BadDot.OnLoseGame += ShowInterstitialAd;
+//        }
 
-        void Awake()
-        {
-            if (Instance != null)
-            {
-                Destroy(gameObject);
-                return;
-            }
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+//        void Awake()
+//        {
+//            if (Instance != null)
+//            {
+//                Destroy(gameObject);
+//                return;
+//            }
+//            Instance = this;
+//            DontDestroyOnLoad(gameObject);
 
-            coinsRVButton.onClick.AddListener(delegate { ShowRewardedAd(COINS_PLACEMENT); });
-            shieldRVButton.onClick.AddListener(delegate { ShowRewardedAd(SHIELD_PLACEMENT); });
-        }
+//            coinsRVButton.onClick.AddListener(delegate { ShowRewardedAd(COINS_PLACEMENT); });
+//            shieldRVButton.onClick.AddListener(delegate { ShowRewardedAd(SHIELD_PLACEMENT); });
+//        }
 
-        private void SdkInitializationCompletedEvent()
-        {
-            IronSource.Agent.validateIntegration();
-            IronSource.Agent.loadInterstitial();
-        }
+//        private void SdkInitializationCompletedEvent()
+//        {
+//            IronSource.Agent.validateIntegration();
+//            IronSource.Agent.loadInterstitial();
+//        }
 
-        void Start()
-        {
-            InitAgents();
-        }
+//        void Start()
+//        {
+//            InitAgents();
+//        }
 
-        void OnApplicationPause(bool isPaused)
-        {
-            IronSource.Agent.onApplicationPause(isPaused);
-        }
+//        void OnApplicationPause(bool isPaused)
+//        {
+//            IronSource.Agent.onApplicationPause(isPaused);
+//        }
 
-        #region Init Ads
-        private void InitAgents()
-        {
-            //For Rewarded Video
-            IronSource.Agent.init(appKey, IronSourceAdUnits.REWARDED_VIDEO);
-            //For Interstitial
-            IronSource.Agent.init(appKey, IronSourceAdUnits.INTERSTITIAL);
-        } 
-        #endregion
+//        #region Init Ads
+//        private void InitAgents()
+//        {
+//            //For Rewarded Video
+//            IronSource.Agent.init(appKey, IronSourceAdUnits.REWARDED_VIDEO);
+//            //For Interstitial
+//            IronSource.Agent.init(appKey, IronSourceAdUnits.INTERSTITIAL);
+//        } 
+//        #endregion
 
-        #region Showing Ads
-        private void ShowInterstitialAd()
-        {
-            if (IronSource.Agent.isInterstitialReady())
-            {
-                IronSource.Agent.showInterstitial();
-            }
-            else
-            {
-                Debug.Log("Ad is not ready");
-                return;
-            }
-        }
+//        #region Showing Ads
+//        private void ShowInterstitialAd()
+//        {
+//            if (IronSource.Agent.isInterstitialReady())
+//            {
+//                IronSource.Agent.showInterstitial();
+//            }
+//            else
+//            {
+//                Debug.Log("Ad is not ready");
+//                return;
+//            }
+//        }
 
-        void ShowRewardedAd(string placement)
-        {
-            if (IronSource.Agent.isRewardedVideoAvailable())
-            {
-                IronSource.Agent.showRewardedVideo(placement);
-            }
-            else
-            {
-                Debug.Log("Ad is not ready");
-                return;
-            }
+//        void ShowRewardedAd(string placement)
+//        {
+//            if (IronSource.Agent.isRewardedVideoAvailable())
+//            {
+//                IronSource.Agent.showRewardedVideo(placement);
+//            }
+//            else
+//            {
+//                Debug.Log("Ad is not ready");
+//                return;
+//            }
 
-        }
-        #endregion
-
-
-        #region Interstitial Callbacks
-        // Interstitial Callbacks
-        // Invoked when the interstitial ad was loaded succesfully.
-        void InterstitialOnAdReadyEvent(IronSourceAdInfo adInfo)
-        {
-            Debug.Log("Ad is ready");
-        }
-        // Invoked when the initialization process has failed.
-        void InterstitialOnAdLoadFailed(IronSourceError ironSourceError)
-        {
-        }
-        // Invoked when the Interstitial Ad Unit has opened. This is the impression indication. 
-        void InterstitialOnAdOpenedEvent(IronSourceAdInfo adInfo)
-        {
-        }
-        // Invoked when end user clicked on the interstitial ad
-        void InterstitialOnAdClickedEvent(IronSourceAdInfo adInfo)
-        {
-        }
-        // Invoked when the ad failed to show.
-        void InterstitialOnAdShowFailedEvent(IronSourceError ironSourceError, IronSourceAdInfo adInfo)
-        {
-        }
-        // Invoked when the interstitial ad closed and the user went back to the application screen.
-        void InterstitialOnAdClosedEvent(IronSourceAdInfo adInfo)
-        {
-            IronSource.Agent.loadInterstitial();
-        }
-        // Invoked before the interstitial ad was opened, and before the InterstitialOnAdOpenedEvent is reported.
-        // This callback is not supported by all networks, and we recommend using it only if  
-        // it's supported by all networks you included in your build. 
-        void InterstitialOnAdShowSucceededEvent(IronSourceAdInfo adInfo)
-        {
-        }
-        #endregion
+//        }
+//        #endregion
 
 
-        #region Rewarded Ads Callbacks
-        // Rewarded Ads Callbacks
-        /************* RewardedVideo AdInfo Delegates *************/
-        // Indicates that there’s an available ad.
-        // The adInfo object includes information about the ad that was loaded successfully
-        // This replaces the RewardedVideoAvailabilityChangedEvent(true) event
-        void RewardedVideoOnAdAvailable(IronSourceAdInfo adInfo)
-        {
-        }
-        // Indicates that no ads are available to be displayed
-        // This replaces the RewardedVideoAvailabilityChangedEvent(false) event
-        void RewardedVideoOnAdUnavailable()
-        {
-        }
-        // The Rewarded Video ad view has opened. Your activity will loose focus.
-        void RewardedVideoOnAdOpenedEvent(IronSourceAdInfo adInfo)
-        {
-        }
-        // The Rewarded Video ad view is about to be closed. Your activity will regain its focus.
-        void RewardedVideoOnAdClosedEvent(IronSourceAdInfo adInfo)
-        {
-        }
-        // The user completed to watch the video, and should be rewarded.
-        // The placement parameter will include the reward data.
-        // When using server-to-server callbacks, you may ignore this event and wait for the ironSource server callback.
-        void RewardedVideoOnAdRewardedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
-        {
-            // Here we reward the player depending on the placement
-            if (placement != null)
-            {
-                string placementName = placement.getPlacementName();
-                string rewardName = placement.getRewardName();
-                int rewardAmount = placement.getRewardAmount();
-            }
-        }
-        // The rewarded video ad was failed to show.
-        void RewardedVideoOnAdShowFailedEvent(IronSourceError error, IronSourceAdInfo adInfo)
-        {
-        }
-        // Invoked when the video ad was clicked.
-        // This callback is not supported by all networks, and we recommend using it only if
-        // it’s supported by all networks you included in your build.
-        void RewardedVideoOnAdClickedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
-        {
-        } 
-        #endregion
+//        #region Interstitial Callbacks
+//        // Interstitial Callbacks
+//        // Invoked when the interstitial ad was loaded succesfully.
+//        void InterstitialOnAdReadyEvent(IronSourceAdInfo adInfo)
+//        {
+//            Debug.Log("Ad is ready");
+//        }
+//        // Invoked when the initialization process has failed.
+//        void InterstitialOnAdLoadFailed(IronSourceError ironSourceError)
+//        {
+//        }
+//        // Invoked when the Interstitial Ad Unit has opened. This is the impression indication. 
+//        void InterstitialOnAdOpenedEvent(IronSourceAdInfo adInfo)
+//        {
+//        }
+//        // Invoked when end user clicked on the interstitial ad
+//        void InterstitialOnAdClickedEvent(IronSourceAdInfo adInfo)
+//        {
+//        }
+//        // Invoked when the ad failed to show.
+//        void InterstitialOnAdShowFailedEvent(IronSourceError ironSourceError, IronSourceAdInfo adInfo)
+//        {
+//        }
+//        // Invoked when the interstitial ad closed and the user went back to the application screen.
+//        void InterstitialOnAdClosedEvent(IronSourceAdInfo adInfo)
+//        {
+//            IronSource.Agent.loadInterstitial();
+//        }
+//        // Invoked before the interstitial ad was opened, and before the InterstitialOnAdOpenedEvent is reported.
+//        // This callback is not supported by all networks, and we recommend using it only if  
+//        // it's supported by all networks you included in your build. 
+//        void InterstitialOnAdShowSucceededEvent(IronSourceAdInfo adInfo)
+//        {
+//        }
+//        #endregion
 
-        void OnDisable()
-        {
-            IronSourceEvents.onSdkInitializationCompletedEvent -= SdkInitializationCompletedEvent;
 
-            //Interstitial 
-            IronSourceInterstitialEvents.onAdReadyEvent -= InterstitialOnAdReadyEvent;
-            IronSourceInterstitialEvents.onAdLoadFailedEvent -= InterstitialOnAdLoadFailed;
-            IronSourceInterstitialEvents.onAdOpenedEvent -= InterstitialOnAdOpenedEvent;
-            IronSourceInterstitialEvents.onAdClickedEvent -= InterstitialOnAdClickedEvent;
-            IronSourceInterstitialEvents.onAdShowSucceededEvent -= InterstitialOnAdShowSucceededEvent;
-            IronSourceInterstitialEvents.onAdShowFailedEvent -= InterstitialOnAdShowFailedEvent;
-            IronSourceInterstitialEvents.onAdClosedEvent -= InterstitialOnAdClosedEvent;
+//        #region Rewarded Ads Callbacks
+//        // Rewarded Ads Callbacks
+//        /************* RewardedVideo AdInfo Delegates *************/
+//        // Indicates that there’s an available ad.
+//        // The adInfo object includes information about the ad that was loaded successfully
+//        // This replaces the RewardedVideoAvailabilityChangedEvent(true) event
+//        void RewardedVideoOnAdAvailable(IronSourceAdInfo adInfo)
+//        {
+//        }
+//        // Indicates that no ads are available to be displayed
+//        // This replaces the RewardedVideoAvailabilityChangedEvent(false) event
+//        void RewardedVideoOnAdUnavailable()
+//        {
+//        }
+//        // The Rewarded Video ad view has opened. Your activity will loose focus.
+//        void RewardedVideoOnAdOpenedEvent(IronSourceAdInfo adInfo)
+//        {
+//        }
+//        // The Rewarded Video ad view is about to be closed. Your activity will regain its focus.
+//        void RewardedVideoOnAdClosedEvent(IronSourceAdInfo adInfo)
+//        {
+//        }
+//        // The user completed to watch the video, and should be rewarded.
+//        // The placement parameter will include the reward data.
+//        // When using server-to-server callbacks, you may ignore this event and wait for the ironSource server callback.
+//        void RewardedVideoOnAdRewardedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
+//        {
+//            // Here we reward the player depending on the placement
+//            if (placement != null)
+//            {
+//                string placementName = placement.getPlacementName();
+//                string rewardName = placement.getRewardName();
+//                int rewardAmount = placement.getRewardAmount();
+//            }
+//        }
+//        // The rewarded video ad was failed to show.
+//        void RewardedVideoOnAdShowFailedEvent(IronSourceError error, IronSourceAdInfo adInfo)
+//        {
+//        }
+//        // Invoked when the video ad was clicked.
+//        // This callback is not supported by all networks, and we recommend using it only if
+//        // it’s supported by all networks you included in your build.
+//        void RewardedVideoOnAdClickedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
+//        {
+//        } 
+//        #endregion
 
-            // RV
-            IronSourceRewardedVideoEvents.onAdOpenedEvent -= RewardedVideoOnAdOpenedEvent;
-            IronSourceRewardedVideoEvents.onAdClosedEvent -= RewardedVideoOnAdClosedEvent;
-            IronSourceRewardedVideoEvents.onAdAvailableEvent -= RewardedVideoOnAdAvailable;
-            IronSourceRewardedVideoEvents.onAdUnavailableEvent -= RewardedVideoOnAdUnavailable;
-            IronSourceRewardedVideoEvents.onAdShowFailedEvent -= RewardedVideoOnAdShowFailedEvent;
-            IronSourceRewardedVideoEvents.onAdRewardedEvent -= RewardedVideoOnAdRewardedEvent;
-            IronSourceRewardedVideoEvents.onAdClickedEvent -= RewardedVideoOnAdClickedEvent;
+//        void OnDisable()
+//        {
+//            IronSourceEvents.onSdkInitializationCompletedEvent -= SdkInitializationCompletedEvent;
 
-            //BadDot.OnLoseGame -= ShowInterstitialAd;
-        }
-    }
-}
+//            //Interstitial 
+//            IronSourceInterstitialEvents.onAdReadyEvent -= InterstitialOnAdReadyEvent;
+//            IronSourceInterstitialEvents.onAdLoadFailedEvent -= InterstitialOnAdLoadFailed;
+//            IronSourceInterstitialEvents.onAdOpenedEvent -= InterstitialOnAdOpenedEvent;
+//            IronSourceInterstitialEvents.onAdClickedEvent -= InterstitialOnAdClickedEvent;
+//            IronSourceInterstitialEvents.onAdShowSucceededEvent -= InterstitialOnAdShowSucceededEvent;
+//            IronSourceInterstitialEvents.onAdShowFailedEvent -= InterstitialOnAdShowFailedEvent;
+//            IronSourceInterstitialEvents.onAdClosedEvent -= InterstitialOnAdClosedEvent;
+
+//            // RV
+//            IronSourceRewardedVideoEvents.onAdOpenedEvent -= RewardedVideoOnAdOpenedEvent;
+//            IronSourceRewardedVideoEvents.onAdClosedEvent -= RewardedVideoOnAdClosedEvent;
+//            IronSourceRewardedVideoEvents.onAdAvailableEvent -= RewardedVideoOnAdAvailable;
+//            IronSourceRewardedVideoEvents.onAdUnavailableEvent -= RewardedVideoOnAdUnavailable;
+//            IronSourceRewardedVideoEvents.onAdShowFailedEvent -= RewardedVideoOnAdShowFailedEvent;
+//            IronSourceRewardedVideoEvents.onAdRewardedEvent -= RewardedVideoOnAdRewardedEvent;
+//            IronSourceRewardedVideoEvents.onAdClickedEvent -= RewardedVideoOnAdClickedEvent;
+
+//            //BadDot.OnLoseGame -= ShowInterstitialAd;
+//        }
+//    }
+//}
