c91256044e6ac28196fbc026756a91a58cf3581d
Shahari93 <shaharilani93@gmail.com>
2023-05-21 23:05:25 +0300

    Check if player has enough coins for upgrades

--- Assets/Scripts/Powerups/PowerupUpgrades/UpgradePowerup.cs
+++ Assets/Scripts/Powerups/PowerupUpgrades/UpgradePowerup.cs
@@ -1,3 +1,4 @@
+using Dots.Coins.Model;
 using System;
 using TMPro;
 using UnityEngine;
@@ -5,14 +6,16 @@ using UnityEngine.UI;
 
 namespace Dots.GamePlay.Powerups.Upgrade
 {
-	public class UpgradePowerup : MonoBehaviour
-	{
+    public class UpgradePowerup : MonoBehaviour
+    {
         private Button upgradeButton;
 
-		[SerializeField] PowerupEffectSO affectedPowerup;
-		[SerializeField] TMP_Text powerupNameText;
-		[SerializeField] TMP_Text powerupDurationText;
-		[SerializeField] TMP_Text upgradeCoinsCostText;
+        [SerializeField] private int coinsCost;
+
+        [SerializeField] PowerupEffectSO affectedPowerup;
+        [SerializeField] TMP_Text powerupNameText;
+        [SerializeField] TMP_Text powerupDurationText;
+        [SerializeField] TMP_Text upgradeCoinsCostText;
 
         private void Awake()
         {
@@ -20,12 +23,28 @@ namespace Dots.GamePlay.Powerups.Upgrade
             upgradeButton.onClick.AddListener(Upgrade);
 
             powerupNameText.text = affectedPowerup.name;
-            powerupDurationText.text = affectedPowerup.powerupDuration.ToString();
+            powerupDurationText.text = string.Format("{0} Seconds", affectedPowerup.powerupDuration.ToString());
+            upgradeCoinsCostText.text = string.Format("{0} Coins", coinsCost);
+        }
+
+        private void Start()
+        {
+            upgradeButton.interactable = CheckIfUpgradeable();
+        }
+
+        private bool CheckIfUpgradeable()
+        {
+            if (CoinsModel.CurrentCoinsAmount < coinsCost)
+            {
+                return false;
+            }
+            else
+                return true;
         }
 
         private void Upgrade()
         {
             throw new NotImplementedException();
         }
-    } 
+    }
 }
--- Assets/Scripts/Utilities/SaveAndLoadJson.cs
+++ Assets/Scripts/Utilities/SaveAndLoadJson.cs
@@ -37,34 +37,5 @@ namespace Dots.Utils.SaveAndLoad
                 return null;
             }
         }
-
-        //public static void SaveCoinsToJson()
-        //{
-        //    if (!File.Exists(Application.persistentDataPath + "/CoinsValue.json"))
-        //    {
-        //        File.Create(Application.persistentDataPath + "/CoinsValue.json");
-        //    }
-        //    else
-        //    {
-        //        CoinsData data = new CoinsData();
-        //        data.savedCoinsInJson = CoinsModel.CurrentCoinsAmount;
-        //        string json = JsonUtility.ToJson(data, true);
-        //        File.WriteAllText(Application.persistentDataPath + "/CoinsValue.json", json);
-        //    }
-        //}
-
-        //public static void LoadCoinsFromJson()
-        //{
-        //    if (!File.Exists(Application.persistentDataPath + "/CoinsValue.json"))
-        //    {
-        //        return;
-        //    }
-        //    else
-        //    {
-        //        string Json = File.ReadAllText(Application.persistentDataPath + "/CoinsValue.json");
-        //        CoinsData Data = JsonUtility.FromJson<CoinsData>(Json);
-        //        CoinsModel.CurrentCoinsAmount = Data.savedCoinsInJson;
-        //    }
-        //}
     }
 }
