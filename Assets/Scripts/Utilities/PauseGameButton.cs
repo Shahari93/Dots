ffe67a62ac3eb3e8a6475fdd943070ff1d03f086
Shahari93 <shaharilani93@gmail.com>
2023-05-29 01:17:25 +0300

    Created a pause panel Play button unpause the game

--- Assets/Scripts/Utilities/PauseGameButton.cs
+++ Assets/Scripts/Utilities/PauseGameButton.cs
@@ -1,52 +1,46 @@
-using UnityEngine;
-using Dots.GamePlay.Dot.Bad;
-using System.Threading.Tasks;
-using UnityEngine.UI;
 using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Dots.Utils.Pause
 {
     public class PauseGameButton : MonoBehaviour
     {
+        [SerializeField] Image darkenPanel;
+        [SerializeField] Image pauseGamePanel;
         [SerializeField] Button pauseGameButton;
+        [SerializeField] Button restartGameButton;
+        [SerializeField] Button returnToGameButton;
+        [SerializeField] Button returnToMainMenuButton;
 
         private bool isGamePaused;
 
         private void Awake()
         {
             pauseGameButton.onClick.AddListener(PauseGame);
+            returnToGameButton.onClick.AddListener(UnPauseGame);
         }
 
         private void PauseGame()
         {
-            isGamePaused = !isGamePaused;
-
-            if (isGamePaused)
+            if (!isGamePaused)
             {
+                darkenPanel.gameObject.SetActive(true);
+                pauseGamePanel.gameObject.SetActive(true);
+                isGamePaused = true;
                 Time.timeScale = 0;
-            }
-
-            else
-            {
-                Time.timeScale = 1;
             }
         }
 
-        //void OnEnable()
-        //{
-        //    BadDot.OnLoseGame += SetTimeScale;
-        //}
-
-        //// Made static for other classes that want to call the PauseGame Method
-        //public async static void SetTimeScale()
-        //{
-        //    await Task.Delay(50);
-        //    Time.timeScale = 0f;
-        //}
-
-        //void OnDisable()
-        //{
-        //    BadDot.OnLoseGame -= SetTimeScale;
-        //}
+        private void UnPauseGame()
+        {
+            if (isGamePaused)
+            {
+                darkenPanel.gameObject.SetActive(false);
+                pauseGamePanel.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                isGamePaused = false;
+            }
+        }
     }
 }
