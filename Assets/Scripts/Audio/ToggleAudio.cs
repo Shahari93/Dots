da7f3638b513d6413099463bbe5434bef00eaae7
Shahari93 <shaharilani93@gmail.com>
2023-06-05 14:10:52 +0300

    Working on settings bug

--- Assets/Scripts/Audio/ToggleAudio.cs
+++ Assets/Scripts/Audio/ToggleAudio.cs
@@ -10,41 +10,32 @@ namespace Dots.Audio
         [SerializeField] Toggle musicToggle, sfxToggle;
         private bool musicToggleState, sfxToggleState;
 
-        private void OnEnable()
+        private void Start()
         {
-            if (!PlayerPrefs.HasKey("MusicToggle") && !PlayerPrefs.HasKey("SFXToggle"))
+            if (PlayerPrefs.HasKey("MusicToggle"))
             {
-                PlayerPrefs.SetInt("MusicToggle", Convert.ToInt32(true));
-                PlayerPrefs.SetInt("SFXToggle", Convert.ToInt32(true));
-
                 musicToggleState = Convert.ToBoolean(PlayerPrefs.GetInt("MusicToggle"));
-                sfxToggleState = Convert.ToBoolean(PlayerPrefs.GetInt("SFXToggle"));
+                musicToggle.isOn = musicToggleState;
             }
-
             else
             {
-                musicToggleState = Convert.ToBoolean(PlayerPrefs.GetInt("MusicToggle"));
-                sfxToggleState = Convert.ToBoolean(PlayerPrefs.GetInt("SFXToggle"));
+                musicToggleState = true;
+                musicToggle.isOn = true;
             }
-
-            musicToggle.isOn = musicToggleState;
-            sfxToggle.isOn = sfxToggleState;
         }
 
         public void ToggleMusic()
         {
             AudioManager.Instance.ToggleMusic();
+            musicToggleState = musicToggle.isOn;
+            PlayerPrefs.SetInt("MusicToggle", Convert.ToInt32(musicToggleState));
         }
 
         public void ToggleSFX()
         {
             AudioManager.Instance.ToggleSFX();
-        }
-
-        private void OnDisable()
-        {
-            PlayerPrefs.SetInt("MusicToggle", Convert.ToInt32(musicToggle.isOn));
-            PlayerPrefs.SetInt("SFXToggle", Convert.ToInt32(sfxToggle.isOn));
+            sfxToggleState = sfxToggle.isOn;
+            PlayerPrefs.SetInt("SFX", Convert.ToInt32(sfxToggleState));
         }
     }
 }
