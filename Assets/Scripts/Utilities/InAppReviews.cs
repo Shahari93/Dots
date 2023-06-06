2a57dafa4280b1166f07a5eaecf82e69ca442806
Shahari93 <shaharilani93@gmail.com>
2023-06-06 13:40:53 +0300

    In app review

---
+++ Assets/Scripts/Utilities/InAppReviews.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Google.Play.Review;
+using System.Collections;
+
+namespace Dots.Utils.Reviews
+{
+    public class InAppReviews : MonoBehaviour
+    {
+        private ReviewManager _reviewManager;
+        private PlayReviewInfo _playReviewInfo;
+
+        int loseCount;
+
+        private void Start()
+        {
+            loseCount = PlayerPrefs.GetInt("LoseCount");
+            if (loseCount == 3 || loseCount == 5)
+            {
+                StartCoroutine(RequestReview());
+            }
+        }
+
+        private IEnumerator RequestReview()
+        {
+            _reviewManager = new ReviewManager();
+
+            // Request a reviewInfo Object
+            var requestFlowOperation = _reviewManager.RequestReviewFlow();
+            yield return requestFlowOperation;
+            if (requestFlowOperation.Error != ReviewErrorCode.NoError)
+            {
+                // Log error. For example, using requestFlowOperation.Error.ToString().
+                yield break;
+            }
+
+            _playReviewInfo = requestFlowOperation.GetResult();
+
+            // Launch the InApp Review Flow
+            var launchFlowOperation = _reviewManager.LaunchReviewFlow(_playReviewInfo);
+            yield return launchFlowOperation;
+            _playReviewInfo = null; // Reset the object
+            if (launchFlowOperation.Error != ReviewErrorCode.NoError)
+            {
+                // Log error. For example, using requestFlowOperation.Error.ToString().
+                yield break;
+            }
+        }
+    } 
+}
--- Assets/Scripts/Utilities/MoveToNextScene.cs
+++ Assets/Scripts/Utilities/MoveToNextScene.cs
@@ -4,6 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class MoveToNextScene : MonoBehaviour
 {
+    private static int loseCount;
+    public static int LoseCount
+    {
+        get
+        {
+            return loseCount;
+        }
+        set
+        {
+            loseCount = value;
+        }
+    }
+
     void OnEnable()
     {
         BadDot.OnLoseGame += MoveToLoseGameScene;
@@ -12,6 +25,24 @@ public class MoveToNextScene : MonoBehaviour
     private void MoveToLoseGameScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        // Checking how many times the player lost so we can show him the In-App Review
+        // check if the key exists.
+        // if so, add to count
+        if (PlayerPrefs.HasKey("LoseCount"))
+        {
+            // get the current count
+            loseCount = PlayerPrefs.GetInt("LoseCount");
+            // increment the count
+            loseCount += 1;
+            // set to PlayerPrefs
+            PlayerPrefs.SetInt("LoseCount", loseCount);
+        }
+        // if not, first time launched, add key
+        else
+        {
+            PlayerPrefs.SetInt("LoseCount", 1);
+        }
     }
 
     void OnDisable()
