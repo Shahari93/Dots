cb430b07efa437da91918cae853edc6b5c41f578
Shahari93 <shaharilani93@gmail.com>
2023-07-04 09:49:15 +0300

    Working on the key prefab

---
+++ Assets/Scripts/Features/KeysAndChests/CollectingKey.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Dots.Utilities.Interface.Destroy;
+using Dots.Utilities.Interface.Interaction;
+
+namespace Dots.Feature.KeyAndChest.Key
+{
+    public class CollectingKey : MonoBehaviour, IInteractableObjects, IDestroyableObject
+    {
+        [SerializeField] ParticleSystem particles;
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("Shahar");
+            if (collision.CompareTag("Bounds"))
+            {
+                BehaveWhenInteractWithBounds();
+            }
+        }
+
+        public void BehaveWhenInteractWithBounds()
+        {
+            DisableVisuals();
+        }
+
+        public void BehaveWhenInteractWithPlayer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DisableVisuals()
+        {
+            ShowDestroyParticles(null);
+            gameObject.SetActive(false);
+        }
+
+        public void ShowDestroyParticles(bool? isGoodDot = null)
+        {
+            GameObject particleGO = Instantiate(particles.gameObject, this.transform.position, Quaternion.identity);
+            ParticleSystem particleSystem = particleGO.GetComponent<ParticleSystem>();
+            ParticleSystem.MainModule main = particleSystem.main;
+
+            if (isGoodDot == null)
+            {
+                Color particlesColor = Color.black;
+                main.startColor = particlesColor;
+            }
+
+            particleSystem.Play();
+            Destroy(particleSystem.gameObject, main.duration + 0.1f);
+        }
+    }
+}
