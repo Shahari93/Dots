34b4daa3fcc4301eb2dcfc6c67e57683c2e13d18
Shahari93 <shaharilani93@gmail.com>
2023-07-04 21:12:28 +0300

    Working on tapping a chest to open it

--- Assets/Scripts/Features/KeysAndChests/TapOnChest.cs
+++ Assets/Scripts/Features/KeysAndChests/TapOnChest.cs
@@ -1,75 +1,39 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using static UnityEngine.RuleTile.TilingRuleOutput;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 namespace Dots.Feature.KeyAndChest.Chest.Tap
 {
     public class TapOnChest : MonoBehaviour
     {
-        [SerializeField] GraphicRaycaster m_Raycaster;
+        [SerializeField] GraphicRaycaster m_Raycast;
         [SerializeField] EventSystem m_EventSystem;
         [SerializeField] RectTransform canvasRect;
         PointerEventData m_PointerEventData;
 
-
-        void Update()
+        private void Update()
         {
-
-            //Set up the new Pointer Event
-            m_PointerEventData = new PointerEventData(m_EventSystem);
-            //Set the Pointer Event Position to that of the game object
-            m_PointerEventData.position = this.transform.localPosition;
-
-            //Create a list of Raycast Results
-            List<RaycastResult> results = new List<RaycastResult>();
-
-            //Raycast using the Graphics Raycaster and mouse click position
-            m_Raycaster.Raycast(m_PointerEventData, results);
-
-            if (results.Count > 0) Debug.Log("Hit " + results[0].gameObject.name);
-
+            CheckIfPlayerTappedOnChest();
         }
 
-        //Camera mainCam;
-
-        //private void Awake()
-        //{
-        //    mainCam = GetComponent<Camera>();
-        //}
-
-        //private void Update()
-        //{
-        //    CheckIfPlayerTappedOnChest();
-        //}
-
-        //private void CheckIfPlayerTappedOnChest()
-        //{
-        //    if (Input.touchCount == 1)
-        //    {
-        //        Touch touch = Input.GetTouch(0);
-        //        Ray ray = mainCam.ScreenPointToRay(touch.position);
-        //        if (touch.phase == TouchPhase.Began)
-        //        {
-        //            CheckForRaycast(ray);
-        //        }
-        //    }
-        //}
-
-        //private void CheckForRaycast(Ray ray)
-        //{
-        //    RaycastHit hit = new RaycastHit();
-        //    // Does the ray intersect any objects excluding the player layer
-        //    if (Physics.Raycast(transform.position, transform.forward, out hit))
-        //    {
-        //        Debug.Log("Did Hit: " + hit.collider.name);
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("Did not Hit");
-        //    }
-        //}
+        private void CheckIfPlayerTappedOnChest()
+        {
+            if (Input.touchCount == 1)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    m_PointerEventData = new PointerEventData(m_EventSystem);
+                    m_PointerEventData.position = touch.position;
+                    List<RaycastResult> results = new List<RaycastResult>();
+                    m_Raycast.Raycast(m_PointerEventData, results);
+                    if (results.Count > 0)
+                    {
+                        Debug.Log("Hit: " + results[0].gameObject.name);
+                    }
+                }
+            }
+        }
     }
 }
