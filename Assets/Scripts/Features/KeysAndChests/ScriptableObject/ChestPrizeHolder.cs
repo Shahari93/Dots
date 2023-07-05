eab62489c65c6b182bd36ad28b6f3fada7005724
Shahari93 <shaharilani93@gmail.com>
2023-07-05 18:38:27 +0300

    Added text underneath the chests sprite

--- Assets/Scripts/Features/KeysAndChests/ScriptableObject/ChestPrizeHolder.cs
+++ Assets/Scripts/Features/KeysAndChests/ScriptableObject/ChestPrizeHolder.cs
@@ -6,12 +6,13 @@ namespace Dots.Feature.KeyAndChest.Prizes.Holder
     public class ChestPrizeHolder : MonoBehaviour
     {
         public ChestPrizeSO prizeSO;
-        //[SerializeField] TMP_Text prizeText;
+        public TMP_Text prizeText;
 
         private void Awake()
         {
             prizeSO.RandomizePrizeAmount();
-            //prizeText.text = string.Format("{0} Coins", prizeSO.prizeAmount);
+            prizeText.text = string.Format("{0} Coins", prizeSO.prizeAmount);
+            prizeText.gameObject.SetActive(false);
         }
     }
 }
